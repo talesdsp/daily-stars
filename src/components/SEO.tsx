5ccb2cf7ec5b32e79e7402f92243bdee5d2dd764
tales <talesdsp@gmail.com>
2020-10-24 22:08:59 -0300

    set application header

---
+++ src/components/SEO.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+
+interface ISEO {
+  siteTitle?: string;
+  description?: string;
+}
+
+export default function SEO({
+  siteTitle = "The House of Tomorrow",
+  description = "Check out your daily dose of awesomes pics of the universe",
+}: ISEO) {
+  return (
+    <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content={description} />
+
+      <title>NASA - {siteTitle}</title>
+
+      <meta name="og:title" content={`NASA - ${siteTitle}`} />
+      <meta
+        property="og:image"
+        content={`https://og-image.now.sh/${encodeURI(
+          `NASA - ${siteTitle}`
+        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+  );
+}
